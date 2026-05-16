f5038ccbfed2e03c730f0fea6ccb7059c8f8a55e
VedantGupta19402 <vedantguptav@gmail.com>
2026-05-17 00:36:18 +0530

    create register and login api

--- backend/src/controllers/controllers.js
+++ backend/src/controllers/controllers.js
@@ -1,6 +1,7 @@
 const userModel=require("../../src/models/user.model.js")
 const jwt=require("jsonwebtoken")
 const bcrypt=require("bcrypt")
+//register
 async function registerUser(req,res){
     const{fullname,email,password}=req.body
     const isuserAlreadyexits=await userModel.findOne({email})
@@ -13,9 +14,10 @@ async function registerUser(req,res){
     const user=await userModel.create({
         fullname,email,password:hashedPassword
     })
+    //register token
     const token=jwt.sign({
         id:user._id
-    },"bbcb741f5efcc32dcac7db680f68235775600a55")
+    },process.env.JWT_SECRET)
     res.cookie(token,"token")
     res.status(201).json({
         message:"user registered succesfully",
@@ -26,6 +28,35 @@ fullname:user.fullname
         }
     })
 }
+//login
+async function loginUser(req,res){
+const {email,password}=req.body
+const user=await userModel.findOne({ email})
+if(!user){
+    return res.status(401).json({
+        message:"Invaild user or password"
+    })
+}
+const isPasswordMatch=bcrypt.compare(password,user.password)
+if(!isPasswordMatch){
+    return res.status(401).json({
+        message:"invaild user or password"
+    })
+}
+//login token
+const token=jwt.sign({
+    id:user._id
+},process.env.JWT_SECRET)
+res.cookie(token,"token")
+res.status(201).json({
+    message:"user logged in succesfully",
+    user:{
+ id:user._id,
+email:user.email,
+fullname:user.fullname
+    }
+})
+}
 module.exports={
-    registerUser
+    registerUser,loginUser
 }
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -2,4 +2,5 @@ const express=require("express");
 const router=express.Router();
 const authcontroller=require("../controllers/controllers")
 router.post('/user/register',authcontroller.registerUser)
+router.post('/user/login',authcontroller.loginUser)
 module.exports=router;
